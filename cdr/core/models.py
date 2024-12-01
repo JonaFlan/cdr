8e0c4ca70ac05683a87a346402d2cbad36f939e2
juliobascu <juliorobertoandres@gmail.com>
2024-12-01 19:41:23 -0300

    jona esclavizador!!!

--- cdr/core/models.py
+++ cdr/core/models.py
@@ -107,6 +107,8 @@ class Noticia(models.Model):
     titulo = models.CharField(max_length=200)
     contenido = models.TextField()
     fecha_publicacion = models.DateField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='noticias/', blank=True, null=True)
+    autor= models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True)
 
     def __str__(self):
         return self.titulo
